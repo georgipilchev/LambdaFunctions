1ba0d5f6ddb1a30fca5bd955a161d8e6ab68b0d5
I531777 <g.pilchev@student.fontys.nl>
2026-06-21 16:06:04 +0200

    added AddUserFunction to src and template, attempted to add user pool trigger set up in github action deployment.

---
+++ src/AddUserFunction/index.mjs
@@ -0,0 +1,30 @@
+import {
+  DynamoDBClient
+} from "@aws-sdk/client-dynamodb";
+
+import {
+  DynamoDBDocumentClient,
+  PutCommand
+} from "@aws-sdk/lib-dynamodb";
+
+const client = DynamoDBDocumentClient.from(
+  new DynamoDBClient({})
+);
+
+export const handler = async (event) => {
+  const attrs = event.request.userAttributes;
+
+  await client.send(
+    new PutCommand({
+      TableName: "UserTable",
+      Item: {
+        UserID: attrs.sub,
+        username: event.userName,
+        email: attrs.email,
+        createdAt: new Date().toISOString()
+      }
+    })
+  );
+
+  return event;
+};
