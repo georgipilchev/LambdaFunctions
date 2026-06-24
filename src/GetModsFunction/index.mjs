ceb34319e26d13bc5107b3ccc71b916b4554d464
I531777 <g.pilchev@student.fontys.nl>
2026-06-24 19:58:27 +0200

    Added s3

--- src/GetModsFunction/index.mjs
+++ src/GetModsFunction/index.mjs
@@ -1,7 +1,8 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-
+import { S3Client } from "@aws-sdk/client-s3";
+const s3 = new S3Client({});
 const client = new DynamoDBClient();
 const docClient = DynamoDBDocumentClient.from(client);
 
