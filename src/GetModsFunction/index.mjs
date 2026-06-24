6907efd59654d01ecb25eb013528f199bf97ac59
I531777 <g.pilchev@student.fontys.nl>
2026-06-24 20:06:39 +0200

    forgot command i am going to crash out

--- src/GetModsFunction/index.mjs
+++ src/GetModsFunction/index.mjs
@@ -1,7 +1,7 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { S3Client } from "@aws-sdk/client-s3";
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
 const s3 = new S3Client({});
 const client = new DynamoDBClient();
 const docClient = DynamoDBDocumentClient.from(client);
